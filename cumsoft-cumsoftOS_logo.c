efeca0bcd900ddf23f3bf1cf4e304dac898106f1
Cumsoft™ <97250816+cumsoft@users.noreply.github.com>
2023-01-13 15:13:49 -0500

    Create cumsoft-cumsoftOS_logo.c

---
+++ cumsoft-cumsoftOS_logo.c
@@ -0,0 +1,19 @@
+// Example OS program
+#include <iostream>
+#include <string>
+
+int main()
+{
+  puts(".                                                                     ");
+  puts("                                                 O~~  O~~      O~~~~       O~~ ~~  ");
+  puts("                                               O~     O~~    O~~    O~~  O~~    O~~");
+  puts("   O~~~O~~  O~~O~~~ O~~ O~~  O~~~~    O~~    O~O~ O~O~O~ O~O~~        O~~ O~~  ");
+  puts(" O~~   O~~  O~~ O~~  O~  O~~O~~     O~~  O~~   O~~    O~~  O~~        O~~   O~~ ");
+  puts("O~~    O~~  O~~ O~~  O~  O~~  O~~~ O~~    O~~  O~~    O~~  O~~        O~~      O~~ ");
+  puts(" O~~   O~~  O~~ O~~  O~  O~~    O~~ O~~  O~~   O~~    O~~    O~~     O~~ O~~    O~~");
+  puts("   O~~~  O~~O~~O~~~  O~  O~~O~~ O~~   O~~      O~~     O~~     O~~~~       O~~ ~~");
+  puts(".                                                       .");
+  
+  return 0;
+}
+
